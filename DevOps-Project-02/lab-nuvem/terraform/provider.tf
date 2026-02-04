a51a1548b150c1d486907c59aa45140700e27081
felipesalesoliveira <fesales.oliveira@gmail.com>
2026-02-04 15:17:44 -0300

    feat: atualização Project-02

--- DevOps-Project-02/lab-nuvem/terraform/provider.tf
+++ DevOps-Project-02/lab-nuvem/terraform/provider.tf
@@ -9,6 +9,12 @@ terraform {
     }
 
   }
+  backend "azurerm" {
+    resource_group_name  = "lab_project_02"         # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "storage-account"        # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "container-storage"      # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
 }
 
 provider "azurerm" {
